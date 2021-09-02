1c3828dd65779b9e31567c4f3b2cad55e43dc5cf
Muhammad-Hunain <hunain29.mem@gmail.com>
2021-09-02 14:26:55 +0500

    storage updated

--- app.js
+++ app.js
@@ -1,29 +1,79 @@
-
 const submit = () =>{
 var name = document.getElementById('name').value;
 var email = document.getElementById('email').value;
-var myfile = document.getElementById('myfile').value;
+var myfile = document.getElementById('select').value;
+if(name=="" || email=="" ){
+    alert("Please enter the value");
+}
+else{
 var obj ={
     name:name,
     email:email,
-    myfile :myfile,
+    myfile:files.name,
 }
-
-
-
 firebase.database().ref(`/${name}`).set(obj)
-
+alert("Your Form Submitted Successfully");
 }
 
+document.getElementById('name').value = "";
+document.getElementById('email').value ="";
+document.getElementById('select').removeAttribute('disabled')
+document.getElementById('upload').setAttribute('disabled',true)
+}
 
+var files = [];
 
+const select= () =>{
+    var input = document.createElement('input');
+    input.type='file';
+    input.accept='image/*';
 
+    input.onchange=(d1)=>{
+        files=d1.target.files[0];
+        console.log(files);
+    document.getElementById('upload').removeAttribute('disabled')
+    document.getElementById('select').setAttribute('disabled',true)
+    }
+    input.click()
+}
 
+const upload=() =>{
+    var refTask = firebase.storage().ref();
+    var uploadTask = refTask.child(`images/${files.name}`).put(files) 
 
+    uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+    }
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    // getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+    //   console.log('File available at', downloadURL);
+    // });
+    var Url_File = uploadTask.snapshot.ref.getDownloadURL().then(function (URL) {
+      alert("your CV uploaded")
+      console.log('File available at', URL);
+    
+  });
+  }
+);
 
-
-
-
+}
 
 
 
--- firbase.js
+++ firbase.js
@@ -1,12 +1,13 @@
 
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyAtBkSYbp7y3dX2ubjKfcdD9oIgYWOZ734",
-    authDomain: "job-form-81145.firebaseapp.com",
-    projectId: "job-form-81145",
-    storageBucket: "job-form-81145.appspot.com",
-    messagingSenderId: "1029230203515",
-    appId: "1:1029230203515:web:eaf1988d4e1711ea3708de"
-  };
+ const firebaseConfig = {
+  apiKey: "AIzaSyAtBkSYbp7y3dX2ubjKfcdD9oIgYWOZ734",
+  authDomain: "job-form-81145.firebaseapp.com",
+  databaseURL: "https://job-form-81145-default-rtdb.firebaseio.com",
+  projectId: "job-form-81145",
+  storageBucket: "job-form-81145.appspot.com",
+  messagingSenderId: "1029230203515",
+  appId: "1:1029230203515:web:eaf1988d4e1711ea3708de"
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
